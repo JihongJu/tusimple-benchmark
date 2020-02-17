3ba65f73414fdfd2c0966f272e8679592c6cb39d
ju <jihong.ju@tomtom.com>
2020-02-17 12:03:16 +0100

    Add a check for the order of the two input arguments

--- evaluate/lane.py
+++ evaluate/lane.py
@@ -23,22 +23,27 @@ class LaneEval(object):
     def line_accuracy(pred, gt, thresh):
         pred = np.array([p if p >= 0 else -100 for p in pred])
         gt = np.array([g if g >= 0 else -100 for g in gt])
-        return np.sum(np.where(np.abs(pred - gt) < thresh, 1., 0.)) / len(gt)
+        return np.sum(np.where(np.abs(pred - gt) < thresh, 1.0, 0.0)) / len(gt)
 
     @staticmethod
     def bench(pred, gt, y_samples, running_time):
         if any(len(p) != len(y_samples) for p in pred):
-            raise Exception('Format of lanes error.')
+            raise Exception("Format of lanes error.")
         if running_time > 200 or len(gt) + 2 < len(pred):
-            return 0., 0., 1.
-        angles = [LaneEval.get_angle(np.array(x_gts), np.array(y_samples)) for x_gts in gt]
+            return 0.0, 0.0, 1.0
+        angles = [
+            LaneEval.get_angle(np.array(x_gts), np.array(y_samples)) for x_gts in gt
+        ]
         threshs = [LaneEval.pixel_thresh / np.cos(angle) for angle in angles]
         line_accs = []
-        fp, fn = 0., 0.
-        matched = 0.
+        fp, fn = 0.0, 0.0
+        matched = 0.0
         for x_gts, thresh in zip(gt, threshs):
-            accs = [LaneEval.line_accuracy(np.array(x_preds), np.array(x_gts), thresh) for x_preds in pred]
-            max_acc = np.max(accs) if len(accs) > 0 else 0.
+            accs = [
+                LaneEval.line_accuracy(np.array(x_preds), np.array(x_gts), thresh)
+                for x_preds in pred
+            ]
+            max_acc = np.max(accs) if len(accs) > 0 else 0.0
             if max_acc < LaneEval.pt_thresh:
                 fn += 1
             else:
@@ -50,51 +55,66 @@ class LaneEval(object):
         s = sum(line_accs)
         if len(gt) > 4:
             s -= min(line_accs)
-        return s / max(min(4.0, len(gt)), 1.), fp / len(pred) if len(pred) > 0 else 0., fn / max(min(len(gt), 4.) , 1.)
+        return (
+            s / max(min(4.0, len(gt)), 1.0),
+            fp / len(pred) if len(pred) > 0 else 0.0,
+            fn / max(min(len(gt), 4.0), 1.0),
+        )
 
     @staticmethod
     def bench_one_submit(pred_file, gt_file):
         try:
             json_pred = [json.loads(line) for line in open(pred_file).readlines()]
         except BaseException as e:
-            raise Exception('Fail to load json file of the prediction.')
+            raise Exception("Fail to load json file of the prediction.")
         json_gt = [json.loads(line) for line in open(gt_file).readlines()]
         if len(json_gt) != len(json_pred):
-            raise Exception('We do not get the predictions of all the test tasks')
-        gts = {l['raw_file']: l for l in json_gt}
-        accuracy, fp, fn = 0., 0., 0.
+            raise Exception("We do not get the predictions of all the test tasks")
+        gts = {l["raw_file"]: l for l in json_gt}
+        accuracy, fp, fn = 0.0, 0.0, 0.0
         for pred in json_pred:
-            if 'raw_file' not in pred or 'lanes' not in pred:
-                raise Exception('raw_file or lanes not in some predictions.')
-            raw_file = pred['raw_file']
-            pred_lanes = pred['lanes']
+            if "raw_file" not in pred or "lanes" not in pred:
+                raise Exception("raw_file or lanes not in some predictions.")
+            raw_file = pred["raw_file"]
+            pred_lanes = pred["lanes"]
             run_time = 50
             if raw_file not in gts:
-                raise Exception('Some raw_file from your predictions do not exist in the test tasks.')
+                raise Exception(
+                    "Some raw_file from your predictions do not exist in the test tasks."
+                )
             gt = gts[raw_file]
-            gt_lanes = gt['lanes']
-            y_samples = gt['h_samples']
+            gt_lanes = gt["lanes"]
+            y_samples = gt["h_samples"]
             try:
                 a, p, n = LaneEval.bench(pred_lanes, gt_lanes, y_samples, run_time)
             except BaseException as e:
-                raise Exception('Format of lanes error.')
+                raise Exception("Format of lanes error.")
             accuracy += a
             fp += p
             fn += n
         num = len(gts)
         # the first return parameter is the default ranking parameter
-        return json.dumps([
-            {'name': 'Accuracy', 'value': accuracy / num, 'order': 'desc'},
-            {'name': 'FP', 'value': fp / num, 'order': 'asc'},
-            {'name': 'FN', 'value': fn / num, 'order': 'asc'}
-        ])
+        return json.dumps(
+            [
+                {"name": "Accuracy", "value": accuracy / num, "order": "desc"},
+                {"name": "FP", "value": fp / num, "order": "asc"},
+                {"name": "FN", "value": fn / num, "order": "asc"},
+            ]
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
+    from pathlib import Path
+
     try:
         if len(sys.argv) != 3:
-            raise Exception('Invalid input arguments')
+            raise Exception("Invalid input arguments")
+        label_file = sys.argv[2]
+        if "label" not in Path(label_file).name:
+            raise ValueError(
+                f"{label_file} is not a label file. Check the order of the two input arguments."
+            )
         print(LaneEval.bench_one_submit(sys.argv[1], sys.argv[2]))
     except Exception as e:
         print(e)
